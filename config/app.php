76b2002cf40575f8c07c7f2da1f56ad3cf135ce5
Denis_Lyubavin <lyubavin.denis@diol-it.ru>
2021-06-02 16:02:59 +0300

    Поставил некоторые пакеты

--- config/app.php
+++ config/app.php
@@ -165,6 +165,8 @@ return [
         /*
          * Package Service Providers...
          */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\Elfinder\ElfinderServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -174,6 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
 
     ],
 
