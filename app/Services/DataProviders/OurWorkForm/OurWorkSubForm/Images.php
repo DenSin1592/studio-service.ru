b17b21271260615bc3e13088280399e4f654579a
Denis Lyubavin <lyubavin.denis@diol-it.ru>
2021-07-28 19:32:40 +0300

    Переделал чутка subforms - считаю упростил реализацию

--- app/Services/DataProviders/OurWorkForm/OurWorkSubForm/Images.php
+++ app/Services/DataProviders/OurWorkForm/OurWorkSubForm/Images.php
@@ -4,14 +4,11 @@ namespace App\Services\DataProviders\OurWorkForm\OurWorkSubForm;
 
 use App\Http\Controllers\Admin\Relations\OurWorks\ImagesController;
 use App\Services\DataProviders\BaseSubForm;
-use App\Services\Eloquent\CollectionExtractor;
 use App\Services\Repositories\OurWork\OurWorkImage\OurWorkImageRepository;
 use Illuminate\Database\Eloquent\Model;
 
 class Images extends BaseSubForm
 {
-    use CollectionExtractor;
-
     protected const SUB_FORM_NAME = ImagesController::RELATIONS_NAME;
 
     public function __construct(
@@ -20,13 +17,7 @@ class Images extends BaseSubForm
 
     public function provideData(Model $model, array $oldInput): array
     {
-        $models = $this->extractFromArray(
-             fn() => $this->repository->newInstance(),
-            $oldInput,
-            self::SUB_FORM_NAME,
-            $this->repository->allForModel($model)
-        );
-
+        $models = $this->repository->allForModel($model);
         return [self::SUB_FORM_NAME => $models];
     }
 }
