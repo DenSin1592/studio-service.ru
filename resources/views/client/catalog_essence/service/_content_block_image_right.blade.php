4cbcb0d42184dfc2d7750e7c4d4b2d57a3db9dc7
Denis Lyubavin <lyubavin.denis@diol-it.ru>
2021-08-25 20:04:46 +0300

    Правки по текстовым блокам

--- resources/views/client/catalog_essence/service/_content_block_image_right.blade.php
+++ resources/views/client/catalog_essence/service/_content_block_image_right.blade.php
@@ -3,21 +3,19 @@
         <div class="row">
             <div class="col-xxl-10 offset-xxl-1">
                 <div class="row">
+                    <div class="section-media-container col-12 col-md-6 col-xxl-6 order-md-1">
+                        <div class="section-thumbnail">
+                            <img src={{$element->getImgPath('image', 'main', 'no-image-500x500.png')}} width="648" height="347" alt="" class="section-media">
+                        </div>
+                    </div>
 
-                    <div class="section-typography-container col-12 col-md-6 col-xxl-6">
+                    <div class="section-typography-container col-12 col-md-6 col-xxl-6 order-md-0">
                         <div class="section-header">
                             <div class="section-title title-h1">{{$element->name}}</div>
                         </div>
 
                         <div class="section-description">{{$element->content}}</div>
                     </div>
-
-                    <div class="section-media-container col-12 col-md-6 col-xxl-6">
-                        <div class="section-thumbnail">
-                            <img src="{{$element->getImgPath('image', 'main', 'no-image-500x500.png')}}" width="619" height="425" alt="" class="section-media">
-                        </div>
-                    </div>
-
                 </div>
             </div>
         </div>
