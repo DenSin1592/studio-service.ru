8eeeb1c636c92b12569ee4b709f5f1d6b786a5de
Denis Lyubavin <lyubavin.denis@diol-it.ru>
2021-08-27 12:44:31 +0300

    Правки

--- resources/views/admin/essence/reviews/_form_fields.blade.php
+++ resources/views/admin/essence/reviews/_form_fields.blade.php
@@ -30,15 +30,5 @@
     )
 )
 <hr>
-{!!  Form::tbFormGroupOpen('review_date') !!}
-    {!! Form::tbLabel('review_date', trans('validation.attributes.review_date')) !!}
-    {!! Form::tbText(
-        'review_date',
-        !empty($formData[$essenceName]->review_date) ? date( "d.m.Y H:i:s" , strtotime($formData[$essenceName]->review_date)) : '',
-        ['data-datetimepicker' => json_encode(['format' => "d.m.Y H:i:s"])]
-    )
-    !!}
-
-{!! Form::tbFormGroupClose() !!}
 
 @include('admin.shared._model_timestamps', ['model' => $formData[$essenceName]])
