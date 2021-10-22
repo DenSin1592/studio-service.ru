ce9df9614f2fafc9758c851cee85595bd1157a59
atemiks <atemiks@gmail.com>
2021-10-22 09:56:23 +0300

    Добавил отступы между проектами на странице проектов

--- resources/views/client/our_work_page/show.blade.php
+++ resources/views/client/our_work_page/show.blade.php
@@ -35,60 +35,62 @@
     </section>
 
 
-    <section class="section-projects m-0">
+    <section class="section-projects section-purple-dark m-0">
+        <div class="projects-grid row">
 
-        @foreach($modelList as $element)
-
-            <div class="card-project ">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xxl-10 offset-xxl-1">
+            @foreach($modelList as $element)
+                <div class="project-item col-12">
+                    <div class="card-project ">
+                        <div class="container">
                             <div class="row">
-                                <div class="card-project-media-container col-12 col-md-6 order-md-1">
-                                    <div class="card-project-thumnbnail">
-                                        <img
-                                            src="{{{ $element->getImgPath('preview_image', 'main', 'no-image-800x800.png') }}}"
-                                            alt="{{$element->name}}s"
-                                            class="card-project-media">
-                                    </div>
-                                </div>
-
-                                <div
-                                    class="card-project-typography-container col-12 col-md-6 order-md-0">
-                                    <a href="{{$element->url}}"
-                                       class="card-project-title title-h3">{{$element->name}}</a>
-
-                                    <div class="card-project-description">{{$element->preview}}</div>
-
-                                    @if($element->services->count() > 0)
-                                        <div class="card-project-services-block">
-                                            <div class="card-project-services-title">Оказанные услуги</div>
-
-                                            <ul class="card-project-services-list list-unstyled check-list">
-
-                                                @foreach($element->services as $elem)
-                                                    <li class="list-item">
-                                                        <a href="{{ $elem->url}}"
-                                                           class="list-link">{{$elem->name}}</a>
-                                                    </li>
-                                                @endforeach
-
-                                            </ul>
+                                <div class="col-xxl-10 offset-xxl-1">
+                                    <div class="row">
+                                        <div class="card-project-media-container col-12 col-md-6 order-md-1">
+                                            <div class="card-project-thumnbnail">
+                                                <img
+                                                    src="{{{ $element->getImgPath('preview_image', 'main', 'no-image-800x800.png') }}}"
+                                                    alt="{{$element->name}}s"
+                                                    class="card-project-media">
+                                            </div>
                                         </div>
-                                    @endif
-
-                                    <a href="{{ $element->url}}"
-                                       class="card-project-cta btn btn-outline-secondary">Более подробно
-                                        о проекте</a>
+        
+                                        <div
+                                            class="card-project-typography-container col-12 col-md-6 order-md-0">
+                                            <a href="{{$element->url}}"
+                                               class="card-project-title title-h3">{{$element->name}}</a>
+        
+                                            <div class="card-project-description">{{$element->preview}}</div>
+        
+                                            @if($element->services->count() > 0)
+                                                <div class="card-project-services-block">
+                                                    <div class="card-project-services-title">Оказанные услуги</div>
+        
+                                                    <ul class="card-project-services-list list-unstyled check-list">
+        
+                                                        @foreach($element->services as $elem)
+                                                            <li class="list-item">
+                                                                <a href="{{ $elem->url}}"
+                                                                   class="list-link">{{$elem->name}}</a>
+                                                            </li>
+                                                        @endforeach
+        
+                                                    </ul>
+                                                </div>
+                                            @endif
+        
+                                            <a href="{{ $element->url}}"
+                                               class="card-project-cta btn btn-outline-secondary">Более подробно
+                                                о проекте</a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            @endforeach
 
-        @endforeach
-        
+        </div>
     </section>
 
     @include('client.shared._section_social')
