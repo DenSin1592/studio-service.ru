df9baefdd25c22fcd33d7f70704366961d84a557
Denis Lyubavin <lyubavin.denis@diol-it.ru>
2021-11-09 12:39:57 +0300

    Убрал дублирующийся запрос

--- app/Services/Composers/ClientTopMenuComposer.php
+++ app/Services/Composers/ClientTopMenuComposer.php
@@ -5,18 +5,26 @@ namespace App\Services\Composers;
 use App\Services\Composers\Features\NodeMenuBuilder;
 use App\Services\Repositories\Node\NodeRepository;
 
+
 class ClientTopMenuComposer
 {
     use NodeMenuBuilder;
+
+    private array $topMenu;
+
 
     public function __construct(
         private NodeRepository $repository
     ){}
 
+
     public function compose($view)
     {
-        $nodeList = $this->repository->treePublishedTopMenu();
-        $topMenu = $this->buildMenu($nodeList);
-        $view->with('topMenu', $topMenu);
+        if(empty($this->topMenu)){
+            $nodeList = $this->repository->treePublishedTopMenu();
+            $this->topMenu = $this->buildMenu($nodeList);
+        }
+
+        $view->with('topMenu', $this->topMenu);
     }
 }
