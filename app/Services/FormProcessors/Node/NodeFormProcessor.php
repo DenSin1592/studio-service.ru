d9a0c4ea9d65adcff9802039f178af8dab2efeb3
Denis Lyubavin <lyubavin.denis@diol-it.ru>
2022-02-14 14:57:07 +0300

    Реализовать укорачивание сформированного из названия alias
    https://yougile.com/team/f9ff4aab69a3/#chat:f90f418d78e3

--- app/Services/FormProcessors/Node/NodeFormProcessor.php
+++ app/Services/FormProcessors/Node/NodeFormProcessor.php
@@ -7,23 +7,6 @@ use App\Services\FormProcessors\Features\AutoAlias;
 
 final class NodeFormProcessor extends BaseFormProcessor
 {
-    use AutoAlias;
 
-    protected function prepareInputData(array $data)
-    {
-        $data = $this->setAutoAlias($data);
-
-        if (empty($data['parent_id'])) {
-            $data['parent_id'] = null;
-        }
-
-        if (isset($data['type'])) {
-            $typeObject = \TypeContainer::getTypeList()[$data['type']];
-            if ($typeObject->getUnique()) {
-                $data['alias'] = null;
-            }
-        }
-
-        return parent::prepareInputData($data);
-    }}
+}
 
