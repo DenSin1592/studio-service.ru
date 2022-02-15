e17a3cf554d46d9b982e4c292066afdd25f03010
Denis Lyubavin <lyubavin.denis@diol-it.ru>
2022-02-15 17:29:03 +0300

    Вывел HTML-код для "Было-стало"
    нужно сделать возможность вставлять Было-СТало куда угодно (по тексту). Для этого просто сделать блок: вставить картинку ДО, картинку После и нажать кнопку Сформировать - выходит код, копируешь его и вставляешь в нужное место в коде
    
    https://yougile.com/team/f9ff4aab69a3/#chat:96c86b1410ea

--- resources/views/admin/essence/before_after_images/_form_fields.blade.php
+++ resources/views/admin/essence/before_after_images/_form_fields.blade.php
@@ -13,4 +13,22 @@
 
 <hr>
 
+@if($formData[$essenceName]->exists)
+    <div class="form-group">
+        <label for="" class="control-label">Код</label>
+        <textarea class="form-control" cols="50" rows="10" disabled>{!!
+'<div class="gallery-header-container d-flex flex-column justify-content-center">
+    <div class="gallery-description">'. $formData[$essenceName]->description .'</div>
+</div>
+
+<div class="twentytwenty-cover">
+    <div class="twentytwenty-block">
+        <img src="'.$formData[$essenceName]->getImgPath('image_before', 'main').'" class="twentytwenty-media" alt="">
+        <img src="'.$formData[$essenceName]->getImgPath('image_after', 'main').'" class="twentytwenty-media" alt="">
+    </div>
+</div>'
+!!}</textarea>
+    </div>
+@endif
+
 @include('admin.shared._model_timestamps', ['model' => $formData[$essenceName]])
